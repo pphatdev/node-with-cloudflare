93126859722e05f4b5be331b6a89e460a9356299
pphatdev <pphat.kits@gmail.com>
2025-07-19 09:30:30 +0700

    refactor: Simplify Error interface and enhance Response class methods

--- src/libs/utils/response.ts
+++ src/libs/utils/response.ts
@@ -6,13 +6,9 @@ interface Success {
     data: Array<Object> | Object;
 }
 
-interface Error {
-    status: number;
-    success: boolean;
-    message: string;
-    version: string;
-    reason?: string;
-}
+interface Error extends Omit<Success, 'data'> {}
+
+interface PaginatedResponse extends Success { total: number; }
 
 interface ResponseHandler {
     success: (data: Array<Object> | Object, status?: number, message?: string) => Success;
@@ -29,15 +25,26 @@ export class Response implements ResponseHandler {
             message: message,
             success: true,
             data
-        }
+        };
     }
 
     error(message: string | string[], status = 400): Error {
         return {
             status,
-            version: Response.VERSION,
             success: false,
+            version: Response.VERSION,
             message: Array.isArray(message) ? message[0] : message
+        };
+    }
+
+    paginate(data: Array<Object> | Object, total: number, status = 200, message = "Request was successful"): PaginatedResponse {
+        return {
+            status,
+            version: Response.VERSION,
+            message: message,
+            success: true,
+            total: total,
+            data: Array.isArray(data) ? data : [data],
         };
     }
 }
