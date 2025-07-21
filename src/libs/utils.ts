dd7784f1229121bbf219b6aedca5b6549a1392f9
pphatdev <pphat.kits@gmail.com>
2025-07-21 13:41:40 +0700

    refactor: Remove unused getTotal function and clean up utils.ts

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,15 +1,11 @@
-import { Context } from "hono";
-import { like, count } from "drizzle-orm";
-
-export const getTotal = async (c: Context, table: any, where: any) => {
-    const db = c.get("db");
-    // Get total count
-    const countQuery = db.select({ count: count() })
-        .from(table)
-        .where(where);
-
-    const { results: countResults } = await countQuery.run();
-    const total = countResults?.[0]?.['count(*)'] || 0;
-
-    return total;
+class Utils {
+    /**
+     * Utility function to parse JSON strings into arrays.
+     * @param {data} - The data to parse, can be a JSON string or an array.
+     * @returns The parsed array if the input is a JSON string, or the input itself if it's already an array.
+    */
+    static toJSONParse = (data: any) => Array.isArray(data) ? data : JSON.parse(data || "[]");
 }
+
+export const { toJSONParse } = Utils;
+export default Utils;
