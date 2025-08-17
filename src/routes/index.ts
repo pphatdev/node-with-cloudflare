359393aa520f08f1379539a0f2250812c02ad709
pphatdev <leatsophat02@gmail.com>
2025-08-17 16:39:08 +0700

    Update path endpoit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Hono } from 'hono';
+import { Env } from '../middlewares/drizzle';
 import WebRoutes from './v1/web/web';
 import ApiRoutes from './v1/api/api';
-import { Env } from '../middlewares/drizzle';
 const app = new Hono();
 
 app.route('/', WebRoutes);
-app.route('/v1', ApiRoutes);
+app.route('/v1/api', ApiRoutes);
 
 export default {
     fetch: (request: Request, env: Env, ctx: any) => {
