05cb42eba0ab22f9358c0565663829425cafd311
pphatdev <leatsophat02@gmail.com>
2025-08-17 21:01:57 +0700

    Update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,5 @@
-import * as Projects from "./schemas/projects";
-
-export const {
-    projects,
-    createProjectsTableQuery,
-} = Projects;
+export { projects, createProjectsTableQuery } from "./schemas/projects";
+export { categories, createCategoriesTableQuery } from "./schemas/categories";
+export { articles, createArticlesTableQuery } from "./schemas/articles";
+export { users, createUsersTableQuery } from "./schemas/users";
+export { sessions, createSessionsTableQuery } from "./schemas/sessions";
