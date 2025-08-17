f970e63055bf7e6cbdedb6a60e1ec4a708c07cf2
pphatdev <leatsophat02@gmail.com>
2025-08-17 16:39:39 +0700

    New Article Types

---
+++ src/types/articles.ts
@@ -0,0 +1,11 @@
+export type Article = {
+    id: number;
+    name: string;
+    description: string;
+    image: string;
+    published: boolean;
+    tags: string[];
+    source: string;
+    authors: string[];
+    languages: string[];
+};
