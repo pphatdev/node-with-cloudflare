1cc497d7c6734f89433cb7472e41e892a930dd28
pphatdev <leatsophat02@gmail.com>
2025-08-17 20:43:01 +0700

    Update Setup Endpoint

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,7 +1,7 @@
 class Utils {
     /**
      * Utility function to parse JSON strings into arrays.
-     * @param {data} - The data to parse, can be a JSON string or an array.
+     * @param {any} [data] The data to parse, can be a JSON string or an array.
      * @returns The parsed array if the input is a JSON string, or the input itself if it's already an array.
     */
     static toJSONParse = (data: any) => Array.isArray(data) ? data : JSON.parse(data || "[]");
--- src/routes/v1/api/api.ts
+++ src/routes/v1/api/api.ts
@@ -1,12 +1,41 @@
 import { Hono } from 'hono';
-import Projects from './projects';
 import { initialize } from '../../../db/setup';
+import Projects from './projects';
+import Articles from './articles';
+import Users from "./users";
+import Sessions from "./sessions";
+import Categories from "./categories";
 import drizzleMiddleware from '../../../middlewares/drizzle';
 
-const app = new Hono().basePath('/api');
+const app = new Hono();
 
 app.use('*', drizzleMiddleware);
 app.post('/setup', initialize);
+
+/**
+ * API routes for Projects
+ */
 app.route('/projects', Projects);
+
+/**
+ * API routes for Articles
+ */
+app.route('/articles', Articles);
+
+/**
+ * API routes for Users
+ */
+app.route('/users', Users);
+
+/**
+ * API routes for Sessions
+ */
+app.route('/sessions', Sessions);
+
+/**
+ * API routes for Categories
+ */
+app.route('/categories', Categories);
+
 
 export default app;
