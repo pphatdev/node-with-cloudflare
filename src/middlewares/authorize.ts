227545b4b994b08140b7361bb651d8baa85e54f5
pphatdev <leatsophat02@gmail.com>
2025-08-27 14:26:13 +0700

    Refactor AuthsMiddleware to include revoked session check during JWT authorization

--- src/middlewares/authorize.ts
+++ src/middlewares/authorize.ts
@@ -1,7 +1,9 @@
-import { Context } from "hono";
+import { Context, Next } from "hono";
 import { jwtVerify } from "jose";
 import { secret } from "../libs/utils";
 import { Response } from "../libs/utils/response";
+import { sessions } from "../db/schemas/sessions";
+import { sql } from 'drizzle-orm';
 const response = new Response();
 
 export class AuthsMiddleware {
@@ -10,7 +12,7 @@ export class AuthsMiddleware {
      * It checks for the Authorization header, verifies the JWT,
      * and sets the user in the context if valid.
      */
-    static async authorize(c: Context, next: () => Promise<void>) {
+    static async authorize(c: Context, next: Next) {
         const authHeader = c.req.header('Authorization');
 
         if (!authHeader) {
@@ -26,6 +28,20 @@ export class AuthsMiddleware {
         try {
             const { payload } = await jwtVerify(token, new TextEncoder().encode(secret));
             c.set('user', payload);
+
+            // check revoked session
+            const db = c.get("db");
+            const { results } = await db
+                .select()
+                .from(sessions)
+                .where(sql`${sessions.token} = ${token} and ${sessions.status} = 1`)
+                .limit(1)
+                .run();
+
+            if (results.length === 0) {
+                return c.json(response.error("Token has been revoked", 401), 401);
+            }
+
             await next();
         } catch (e) {
             return c.json(response.error("Invalid token", 401), 401);
